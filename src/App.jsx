2430ad2e6a1d38bc0c8554584ae0c0df9d9853d5
Nicole Assis <nicassissiqueira@gmail.com>
2026-05-31 18:38:04 -0300

    Atualizações para Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // 👈 Mudou para HashRouter
 import WelcomePage from "./pages/Welcome/WelcomePage";
 import ContactPage from "./pages/Contact/ContactPage";
 import AcademicPage from "./pages/Academic/AcademicPage";
@@ -8,7 +8,8 @@ import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Router basename="/portfolio-nicole"> {/* 👈 Ajustado aqui */}
+    // No HashRouter você NÃO precisa mais do basename="/portfolio-nicole"
+    <Router> 
       <Routes>
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/contact" element={<ContactPage />} />
@@ -22,18 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <Router basename="/portfolio-nicole">
-//       <Routes>
-//         <Route path="/" element={<div style={{ padding: '20px', color: 'black' }}><h1>O React funcionou!</h1></div>} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
